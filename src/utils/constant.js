ebbaf2efec27d7e8624f4d53506fd1c5bc8a1729
akshay-787 <gakshay787@gmail.com>
2024-08-25 22:00:40 +0530

    pro

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,13 +1,5 @@
-
 const ENV = process.env.NODE_ENV; // "development" or "production"
 
-const CONFIG = {
-  development: {
-    API_URL: 'http://localhost:3000',
-  },
-  production: {
-    API_URL: 'https://syncsoft-global.vercel.app',
-  },
-};
+const API_URL = process.env.REACT_APP_API_URL;
 
-export const API_URL = CONFIG[ENV].API_URL;
+export { API_URL };
